75aa34d77615fe4af7f4afc8e38f745b048dd21a
Beau Simensen <beau@dflydev.com>
2014-07-08 22:04:10 -0500

    This just looked weird to me. :-/

--- tests/test.php
+++ tests/test.php
@@ -7,7 +7,8 @@ chdir(__DIR__);
 foreach (glob('*Test.php') as $filename) {
     echo "\n";
     echo basename($filename, 'Test.php')."\n";
-    echo str_repeat('=', strlen(basename($filename, 'Test.php')))."\n\n";
+    echo str_repeat('=', strlen(basename($filename, 'Test.php')))."\n";
+    echo "\n";
     require $filename;
 }
 
